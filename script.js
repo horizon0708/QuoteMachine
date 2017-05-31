ad89684a1bbaa685c110c07435d263e417f9d730
JamesKim <james.kim.nz92@gmail.com>
2017-05-31 18:36:20 +1200

    random color is working, but looks unnatural

--- script.js
+++ script.js
@@ -2,65 +2,126 @@ var isAlternating = true;
 var dataOne; // called when isAlternating = true;
 var dataTwo; // called when isAlternating = false;
 var firstQuote;
+var test;
+var currentHue;
+var nextHue;
 
-$(document).ready(function(){
+$(document).ready(function () {
 	loadInitialQuotes();
-	generateQuote();
+	InitialiseHue();
 });
 
-$('.quotegen').click(function(){
+$('.quotegen').click(function () {
+	fadeColor();
 	generateQuote();
+
 });
 
-function loadInitialQuotes(){
-	dataOne = doIt();
-	dataTwo = doIt();
+function loadInitialQuotes() {
+	quoteLoad().then(function (response) {
+		dataTwo = test;
+	});
+	quoteLoad().then(function (response) {
+		dataOne = test;
+		generateQuote();
+	});
+
+}
+
+//https://stackoverflow.com/questions/967815/how-do-you-fade-in-out-a-background-color-using-jquery
+//https://stackoverflow.com/questions/2171602/settimeout-and-anonymous-function-problem/2171666#2171666
+
+function InitialiseHue() {
+	var h = Math.floor(Math.random() * 359);
+	$('.background').css("background-color", "hsl(" + h + ",50%,75%");
+	$('.color-change').css("color", "hsl(" + h + ",50%,75%");
+	currentHue = h;
+}
+
+
+function fadeColor() {
+	nextHue = Math.floor(Math.random() * 359); // Generates H value of hsl;
+	var d = 1000;
+
+	if (nextHue > currentHue) {
+		var changeValue = Math.abs(nextHue - currentHue) / 1000; //makes all transitions last the same.
+		
+		for (var i = currentHue; i <= nextHue; i = i + changeValue) {
+			d += 1;
+			(function (ii, dd) {
+				setTimeout(function () {
+					$('.background').css("background-color", "hsl(" + ii + ",50%,75%");
+					$('.color-change').css("color", "hsl(" + ii + ",50%,75%");
+				}, dd)
+			})(i, d);
+		}
+	} else {
+		for (var i = currentHue; i >= nextHue; i = i - changeValue) {
+			d += 1;
+			(function (ii, dd) {
+				setTimeout(function () {
+					$('.background').css("background-color", "hsl(" + ii + ",50%,75%");
+					$('.color-change').css("color", "hsl(" + ii + ",50%,75%");
+				}, dd)
+			})(i, d);
+		}
+	}
+
+	currentHue = nextHue;
 }
 
 // calling upon API for every refresh makes it laggy, thus I want to cache them.
 // what I could do, is that I could load two objects at first, then alternate between them
-function generateQuote(){
-	if (isAlternating == true){
-		document.getElementById("quote").innerHTML = '"'+dataOne.quote+'"';
-		document.getElementById("author").innerHTML = '- '+dataOne.author;
-		dataOne = doIt();
-		isAlternating = false;		
-	}
-	else if (!isAlternating){
-		document.getElementById("quote").innerHTML = '"'+dataTwo.quote+'"';
-		document.getElementById("author").innerHTML = '- '+dataTwo.author;
-		dataTwo = doIt();
-		isAlternating = true;	
+function generateQuote() {
+	if (isAlternating == true) {
+		document.getElementById("quote").innerHTML = '"' + dataOne.quote + '"';
+		document.getElementById("author").innerHTML = '- ' + dataOne.author;
+		quoteLoad().then(function (response) {
+			dataOne = test;
+		});
+		isAlternating = false;
+	} else if (!isAlternating) {
+		document.getElementById("quote").innerHTML = '"' + dataTwo.quote + '"';
+		document.getElementById("author").innerHTML = '- ' + dataTwo.author;
+		quoteLoad().then(function (response) {
+			dataTwo = test;
+		});
+		isAlternating = true;
 	}
 }
 // instead of doing this, I want to be able to cache 10 quotes at a time.. but the API isn't giving me an array, but rather just one datum.
 // https://github.com/mdn/js-examples/blob/master/promises-test/index.html
 
 //http://api.jquery.com/jquery.ajax/
-let QuotePromise = new Promise((resolve, reject) =>{
-	doIt();
-	resolve(doIt());
-});
+function quoteLoad() {
+	return new Promise((resolve, reject) => {
 
+		var request = $.ajax({
+			url: "https://andruxnet-random-famous-quotes.p.mashape.com/?cat=famous&count=10/", // The URL to the API. You can get this by clicking on "Show CURL example" from an API profile
+			type: 'GET', // The HTTP Method, can be GET POST PUT DELETE etc
+			data: {}, // Additional parameters here
+			// not too sure how data works..
 
+			dataType: 'json',
+			success: function (data) {
+				//test = data;
+			},
+			error: function (err) {
+				alert(err);
+			},
+			async: true, // this was the issue, the api data loaded AFTER the script had executed - seems depreciated, perhaps there is another way to wait for async data to load?
+			beforeSend: function (xhr) {
+				xhr.setRequestHeader("X-Mashape-Authorization", "dZr0xRJy5MmshBFjcOpyDJx2OyN6p1oxiA3jsnkr4jiyriucuP"); // Enter here your Mashape key
+			}
+		});
 
-function doIt() { 
- var test;
- $.ajax({
-    url: "https://andruxnet-random-famous-quotes.p.mashape.com/?cat=famous&count=10/", // The URL to the API. You can get this by clicking on "Show CURL example" from an API profile
-    type: 'GET', // The HTTP Method, can be GET POST PUT DELETE etc
-    data: {}, // Additional parameters here
-	// not too sure how data works..
+		request.onload = function () {
+			if (request.status === 200) {
+				resolve(test = JSON.parse(request.response));
 
-    dataType: 'json',
-    success: function(data) {
-        test = data;
-        },
-    error: function(err) { alert(err); },
-	async: false, // this was the issue, the api data loaded AFTER the script had executed - seems depreciated, perhaps there is another way to wait for async data to load?
-    beforeSend: function(xhr) {
-    xhr.setRequestHeader("X-Mashape-Authorization", "dZr0xRJy5MmshBFjcOpyDJx2OyN6p1oxiA3jsnkr4jiyriucuP"); // Enter here your Mashape key
-}});
-return test;
+			} else {
+				reject(Error('The quote didn\'t load successfully;'));
+			}
+		}
+	});
 }
-
